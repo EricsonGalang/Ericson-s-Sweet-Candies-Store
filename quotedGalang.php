555a15e7dfab439a7e2209873eea7f09996fa979
Ericson Galang <erixoi2005@gmail.com>
2025-12-04 01:46:04 +0800

    Delete quotedGalang.php

--- quotedGalang.php
+++
@@ -1,9 +0,0 @@
-<?php
-$quotedGalang = [
-    "If life aint it, try some Candies!",
-    "Sweet treats is always the lucky wheat",
-    "Happiness is a choice, choose Candy!"
-];
-$quote = $quotedGalang[array_rand($quotedGalang)];
-?>
-
